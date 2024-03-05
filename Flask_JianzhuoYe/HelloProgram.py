4849e4ee10cda1ab18fff17180c40498676794e5
Jianzhuo Y <147121033+KaCy1123@users.noreply.github.com>
2024-03-05 12:13:36 -0500

    Completed and submitted

--- Flask_JianzhuoYe/HelloProgram.py
+++ Flask_JianzhuoYe/HelloProgram.py
@@ -1,8 +1,8 @@
 '''
 Name: Jianzhuo Ye
-Start Date: 2/26/24
-End Date: 2/26/24
-Description: This program is an introduction to using flask and python
+Start Date: 2/27/24
+End Date: 2/27/24
+Description: This program is an introduction to using flask and python and produces multiple lines on the web page
 '''
 
 from flask import Flask
@@ -12,7 +12,10 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-    return "Hello. This is the main program! <h1>Hello</h1>"
+    output = "Hello. This is the main program! <h1>Hello</h1>";
+    output = output + "<p>This is a paragraph</p>This is not a paragraph";
+    output = output + "<br>This is also not a paragraph<p>This is another paragraph</p>";
+    return output;
 
 if __name__ == "__main__":
     app.run()
---
+++ Flask_JianzhuoYe/RedirectURLFor.py
@@ -0,0 +1,25 @@
+'''
+Name: Jianzhuo Ye
+Start Date: 3/5/24
+End Date: 3/6/24
+Description: This program introduces the use of redirect url
+'''
+
+from flask import Flask,redirect,url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! this is the main page <h1>HELLO</h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("user",name="Jianzhuo Ye")) #Type your name
+
+if __name__ == "__main__":
+    app.run()
