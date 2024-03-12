1294ff82549c2e077a7a0ec1d43ee239b87d3d7d
Jianzhuo Y <147121033+KaCy1123@users.noreply.github.com>
2024-03-12 12:40:15 -0400

    Updated for flask activity - Personal Information

--- Flask_JianzhuoYe/GetandPost.py
+++ Flask_JianzhuoYe/GetandPost.py
@@ -5,7 +5,7 @@ End Date: 3/8/24
 Description: This program uses get and post to print an input on a separate HTML document
 '''
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -15,18 +15,18 @@ def main():
         return render_template('personinfo.html')
     else:
         GetInfo()
-        return render_template('personinfo.html')
+        return render_template('personinfooutput.html')
+        return redirect(url_for("out", output1 = output))
 
 def GetInfo():
     global output
     name = request.form.get('txtname')
     email = request.form.get('txtemail')
     output = "Hello " + name + ". Thank you for your email " + email
-
-@app.route('/output')
-def output():
     
-    return "<h1>" + output + "</h1>"
-
+@app.route('/<output1>')
+def out(output1):
+    return f"{output1}"
+    
 if __name__ == '__main__':
     app.run()
---
+++ Flask_JianzhuoYe/PersonalInformationActivity.py
@@ -0,0 +1,9 @@
+'''
+Name: Jianzhuo Ye
+Start Date: 3/12/23a
+End Date: 3/18/23a
+Description: This program will display a student's schedule from one HTML
+             document to another, and display it in a table
+'''
+
+from flask import Flask,redirect,url_for,
